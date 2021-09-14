c168e1b987758c0c84b22e1cc8ad791fa84ca6c8
bshantam97 <bshantam97@gmail.com>
2021-09-14 14:05:28 -0400

    Completed Harris Corner Detection

--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -64,7 +64,7 @@ int main(int argc, const char *argv[])
         frame.cameraImg = imgGray;
 
         // Implementation of ring buffer
-        if (dataBuffer.size() == 2) {
+        if (dataBuffer.size() == dataBufferSize) {
             // Push back the elements
             dataBuffer.push_back(frame);
 
@@ -81,7 +81,7 @@ int main(int argc, const char *argv[])
 
         // extract 2D keypoints from current image
         std::vector<cv::KeyPoint> keypoints; // create empty feature list for current image
-        std::string detectorType = "SHITOMASI";
+        std::string detectorType = "HARRIS";
 
         //// STUDENT ASSIGNMENT
         //// TASK MP.2 -> add the following keypoint detectors in file matching2D.cpp and enable string-based selection based on detectorType
@@ -90,10 +90,30 @@ int main(int argc, const char *argv[])
         if (detectorType.compare("SHITOMASI") == 0)
         {
             detKeypointsShiTomasi(keypoints, imgGray, false);
-        }
-        else
-        {
-            //...
+        } 
+        
+        else if (detectorType.compare("HARRIS") == 0) {
+            detKeypointsHarris(keypoints, imgGray, true);
+        } 
+        
+        else if (detectorType.compare("FAST") == 0) {
+
+        } 
+        
+        else if (detectorType.compare("BRISK") == 0) {
+
+        } 
+        
+        else if (detectorType.compare("ORB") == 0) {
+
+        } 
+        
+        else if (detectorType.compare("AKAZE") == 0) {
+
+        } 
+        
+        else if (detectorType.compare("SIFT") == 0) {
+
         }
         //// EOF STUDENT ASSIGNMENT
 
--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -1,6 +1,67 @@
 #include <numeric>
 #include "matching2D.hpp"
 
+void detKeypointsHarris (std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis) {
+    // The image input is already grayscale
+    int blockSize = 4; //  The neighborhood size of the Harris corner response
+    int aperture = 3; // For the sobel operator
+    double k = 0.04; // The free parameter in the formula R = eig1*eig2 - k * (eig1 + eig2)
+    int minResponse = 100;
+    cv::Mat dest = cv::Mat::zeros(img.size(), CV_32FC1); // The image type is decided according to the documentation
+    cv::cornerHarris(img, dest, blockSize, aperture, k);
+    
+    // Normalize the image to increase contrast for better feature extract
+    cv::Mat dst_norm, dst_norm_scaled;
+    cv::normalize(dest, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+    // Scales and calculates absolute value and converts the image to 8-bit (0-255)
+    cv::convertScaleAbs(dst_norm, dst_norm_scaled);
+
+    // Now what we do is calculate the local maxima in the harris response matrix and perform an NMS in a local neighborhood around each maxima
+    // The keypoint vector has been given as argument
+    double nmsOverlap;
+    for (size_t i = 0; i < dst_norm_scaled.rows; i++) {
+        for (size_t j = 0; j < dst_norm_scaled.cols; j++) {
+            int response = (int)dst_norm_scaled.at<float>(i,j);
+            if (response > minResponse) {
+
+                cv::KeyPoint harrisKeypoint;
+                harrisKeypoint.pt = cv::Point2f(j,i);
+                harrisKeypoint.response = response; // Actual keypoint response value
+                harrisKeypoint.size = 2*aperture; // Region around keypoint used to form keypoint descriptor
+                // Create boolean overlap variable
+                bool overlap = false;
+                for (auto it = keypoints.begin(); it != keypoints.end(); ++it) {
+                    // Compute the percentage overlap
+                    double kptOverlap = cv::KeyPoint::overlap(harrisKeypoint, *it);
+                    if (kptOverlap > nmsOverlap) {
+                        overlap = true;
+                        if (harrisKeypoint.response > (*it).response) {
+                            *it = harrisKeypoint;
+                            break;
+                        }
+                    }
+                }
+
+                if (!overlap) {
+                    keypoints.push_back(harrisKeypoint);
+                }
+            }
+        }
+    }
+    if (bVis) {
+        std::string windowName = "Harris Keypoints";
+        cv::namedWindow(windowName, 5);
+        cv::Mat visImage = dst_norm_scaled.clone();
+        cv::drawKeypoints(dst_norm_scaled, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        cv::imshow(windowName, visImage);
+        cv::waitKey(0);
+    }
+}
+
+void detKeypointsModern (std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, std::string detectorType, bool bVis = false) {
+
+}
+
 // Find best matches for keypoints in two camera images based on several matching methods
 void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::KeyPoint> &kPtsRef, cv::Mat &descSource, cv::Mat &descRef,
                       std::vector<cv::DMatch> &matches, std::string descriptorType, std::string matcherType, std::string selectorType)
